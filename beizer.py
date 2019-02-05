a195456c5a9a586e66ded352b0722451a9b22636
crescent-igor <31757907+crescent-igor@users.noreply.github.com>
2019-02-05 21:12:49 +0530

    Update beizer.py

--- beizer.py
+++ beizer.py
@@ -1,72 +1,50 @@
-import pygame
 import sys
-import math
-import time
+import pygame
 from pygame import gfxdraw
 
 
-B = [[-1, 3, -3, 1], 
-    [3, -6, 3, 0], 
-    [-3, 3, 0, 0], 
-    [1, 0, 0,0]] 
-      
-C1=[[5],[5],[8],[8]]
-C2=[[3.5],[7],[4],[8]]
+A = [[0, 0, 0, 0]]
+B = [[-1, 3, -3, 1],
+     [3, -6, 3, 0],
+     [-3, 3, 0, 0],
+     [1, 0, 0, 0]]
+Cx = [[50], [50], [80], [80]]
+Cy = [[35], [70], [40], [80]]
 
 
+def mul(A, B):
+    result = [[0, 0, 0, 0]]
 
-def mul1(A,B):
-    result = [[0, 0, 0, 0]] 
-  
-    for i in range(len(A)):  
-        for j in range(len(B[0])): 
-            for k in range(len(B)): 
-                result[i][j] += A[i][k] * B[k][j] 
-  
-    for r in result:
-    return r     
-
-
-def mul2(A,B):
-    result = [[0, 0, 0, 0]] 
-  
-    for i in range(len(A)):  
-        for j in range(len(B[0])): 
-            for k in range(len(B)): 
-                result[i][j] += A[i][k] * B[k][j] 
-  
-    return result[0][0]
+    for i in range(len(A)):
+        for j in range(len(B[0])):
+            for k in range(len(B)):
+                result[i][j] += A[i][k] * B[k][j]
+    return result
 
 
 pygame.init()
+screen = pygame.display.set_mode((640, 480))
+screen.fill((255, 255, 255))
+pygame.display.flip()
 
-while True:
+white = (255, 255, 255)
+
+
+t = 0
+while(t < 1):
+    A[0][0] = t*t*t
+    A[0][1] = t*t
+    A[0][2] = t
+    A[0][3] = 1
+    C = mul(A, B)
+    D1 = mul(C, Cx)
+    D2 = mul(C, Cy)
+    gfxdraw.pixel(screen, round(D1[0][0]), round(D2[0][0]), (0, 0, 0))
+    t = t+0.005
+pygame.display.update()
+
+
+while 1:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit() 
+        if(event.type == pygame.QUIT):
             sys.exit()
-
-
-     
-    screen=pygame.display.set_mode((640,480))
-    color=[(255,0,0),(0,255,0),(0,0,255),(0,0,0),(255,255,255)]
-
-    screen.fill(color[4])
-    t=0
-    tx=0
-    ty=0
-
-    while(t!=1):
-        A=[[t*t*t,t*t,t,1]]
-        r1=mul1(A,B)
-        
-        gfxdraw.pixel(screen,round(tx) , round(ty), (0, 0, 0))
-        t=t+0.1
-        tx=tx+0.1
-        ty=ty+0.1
-
-        
-
-    
-
-    pygame.display.update()
