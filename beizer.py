14a1b9822ac76e0c3bb2b62986cebdf93779fda2
crescent-igor <31757907+crescent-igor@users.noreply.github.com>
2019-02-05 21:20:31 +0530

    Update beizer.py

--- beizer.py
+++ beizer.py
@@ -3,15 +3,6 @@ import pygame
 from pygame import gfxdraw
 
 
-A = [[0, 0, 0, 0]]
-B = [[-1, 3, -3, 1],
-     [3, -6, 3, 0],
-     [-3, 3, 0, 0],
-     [1, 0, 0, 0]]
-Cx = [[50], [50], [80], [80]]
-Cy = [[35], [70], [40], [80]]
-
-
 def mul(A, B):
     result = [[0, 0, 0, 0]]
 
@@ -22,6 +13,26 @@ def mul(A, B):
     return result
 
 
+def beizer(Cx, Cy):
+    B = [[-1, 3, -3, 1], [3, -6, 3, 0], [-3, 3, 0, 0], [1, 0, 0, 0]]
+    t = 0
+    A = [[0, 0, 0, 0]]
+    while(t < 1):
+        A[0][0] = t*t*t
+        A[0][1] = t*t
+        A[0][2] = t
+        A[0][3] = 1
+        C = mul(A, B)
+        D1 = mul(C, Cx)
+        D2 = mul(C, Cy)
+        gfxdraw.pixel(screen, round(D1[0][0]), round(D2[0][0]), (0, 0, 0))
+        t = t+0.005
+
+
+Cx = [[50], [50], [80], [80]]
+Cy = [[35], [70], [40], [80]]
+
+
 pygame.init()
 screen = pygame.display.set_mode((640, 480))
 screen.fill((255, 255, 255))
@@ -29,18 +40,8 @@ pygame.display.flip()
 
 white = (255, 255, 255)
 
+beizer(Cx, Cy)
 
-t = 0
-while(t < 1):
-    A[0][0] = t*t*t
-    A[0][1] = t*t
-    A[0][2] = t
-    A[0][3] = 1
-    C = mul(A, B)
-    D1 = mul(C, Cx)
-    D2 = mul(C, Cy)
-    gfxdraw.pixel(screen, round(D1[0][0]), round(D2[0][0]), (0, 0, 0))
-    t = t+0.005
 pygame.display.update()
 
 
